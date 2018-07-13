9ae1b32ed3e78f76c2166c0ea6ff8b259209b90b
Stephen Damm <shinhalsafar@gmail.com>
2018-07-12 18:55:54 -0600

    - Migrate tests into own plugin.

---
+++ SongBrowserPluginTest/Tests/SongBrowserTestRunner.cs
@@ -0,0 +1,25 @@
+﻿using SongBrowserPlugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SongBrowserPluginTests
+{
+    public class SongBrowserTestRunner
+    {
+        private Logger _log = new Logger("SongBrowserTestRunner");
+
+        public SongBrowserTestRunner()
+        {
+
+        }
+
+        public void RunTests()
+        {
+            _log.Info("Running Song Browser Tests");
+
+            new SongBrowserModelTests().RunTest();
+        }
+    }
+}
