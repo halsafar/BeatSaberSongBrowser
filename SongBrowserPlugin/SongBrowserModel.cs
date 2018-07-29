c7e888fb4ef168b479cf410ba76128502c5d8326
Stephen Damm <shinhalsafar@gmail.com>
2018-07-29 17:09:18 -0600

    Map custom songs into directory tree.

--- SongBrowserPlugin/SongBrowserModel.cs
+++ SongBrowserPlugin/SongBrowserModel.cs
@@ -9,23 +9,46 @@ using UnityEngine;
 
 namespace SongBrowserPlugin
 {
+    class DirectoryNode
+    {
+        public string Key { get; private set; }
+        public Dictionary<String, DirectoryNode> Nodes;
+        public List<StandardLevelSO> Levels;
+
+        public DirectoryNode(String key)
+        {
+            Key = key;
+            Nodes = new Dictionary<string, DirectoryNode>();
+            Levels = new List<StandardLevelSO>();
+        }
+    }
+
     public class SongBrowserModel
     {
+        private const String CUSTOM_SONGS_DIR = "CustomSongs";
+
         public static String LastSelectedLevelId { get; set; }
 
         private Logger _log = new Logger("SongBrowserModel");
         
+        // song_browser_settings.xml
         private SongBrowserSettings _settings;
 
+        // song list management
         private List<StandardLevelSO> _sortedSongs;
         private List<StandardLevelSO> _originalSongs;
         private Dictionary<String, SongLoaderPlugin.OverrideClasses.CustomLevel> _levelIdToCustomLevel;
         private SongLoaderPlugin.OverrideClasses.CustomLevelCollectionSO _gameplayModeCollection;    
         private Dictionary<String, double> _cachedLastWriteTimes;
         private Dictionary<string, int> _weights;
+        private Dictionary<String, DirectoryNode> _directoryTree;
+        private String _currentDirectory = CUSTOM_SONGS_DIR;       
 
         public bool InvertingResults { get; private set; }
 
+        /// <summary>
+        /// Get the settings the model is using.
+        /// </summary>
         public SongBrowserSettings Settings
         {
             get
@@ -34,6 +57,9 @@ namespace SongBrowserPlugin
             }
         }
 
+        /// <summary>
+        /// Get the sorted song list for the current working directory.
+        /// </summary>
         public List<StandardLevelSO> SortedSongList
         {
             get
@@ -42,6 +68,9 @@ namespace SongBrowserPlugin
             }
         }
 
+        /// <summary>
+        /// Map LevelID to Custom Level info.  
+        /// </summary>
         public Dictionary<String, SongLoaderPlugin.OverrideClasses.CustomLevel> LevelIdToCustomSongInfos
         {
             get
@@ -88,7 +117,7 @@ namespace SongBrowserPlugin
         }
 
         /// <summary>
-        /// 
+        /// Easy invert of toggling.
         /// </summary>
         public void ToggleInverting()
         {
@@ -101,7 +130,7 @@ namespace SongBrowserPlugin
         /// </summary>
         public void UpdateSongLists(GameplayMode gameplayMode)
         {
-            String customSongsPath = Path.Combine(Environment.CurrentDirectory, "CustomSongs");
+            String customSongsPath = Path.Combine(Environment.CurrentDirectory, CUSTOM_SONGS_DIR);
             String cachedSongsPath = Path.Combine(customSongsPath, ".cache");
             DateTime currentLastWriteTIme = File.GetLastWriteTimeUtc(customSongsPath);
             IEnumerable<string> directories = Directory.EnumerateDirectories(customSongsPath, "*.*", SearchOption.AllDirectories);
@@ -117,8 +146,9 @@ namespace SongBrowserPlugin
                 _cachedLastWriteTimes[slashed_dir] = (File.GetLastWriteTimeUtc(dir) - Epoch).TotalMilliseconds;
             }
 
-            // Update song Infos
-            this.UpdateSongInfos(gameplayMode);                                
+            // Update song Infos, directory tree, and sort
+            this.UpdateSongInfos(gameplayMode);
+            this.UpdateDirectoryTree(customSongsPath);
             this.ProcessSongList(gameplayMode);                       
         }
 
@@ -129,6 +159,7 @@ namespace SongBrowserPlugin
         {
             _log.Trace("UpdateSongInfos for Gameplay Mode {0}", gameplayMode);
 
+            // Get the level collection from song loader
             SongLoaderPlugin.OverrideClasses.CustomLevelCollectionsForGameplayModes collections = SongLoaderPlugin.SongLoader.Instance.GetPrivateField<SongLoaderPlugin.OverrideClasses.CustomLevelCollectionsForGameplayModes>("_customLevelCollectionsForGameplayModes");
             _gameplayModeCollection = collections.GetCollection(gameplayMode) as SongLoaderPlugin.OverrideClasses.CustomLevelCollectionSO;
             _originalSongs = collections.GetLevels(gameplayMode).ToList();
@@ -136,6 +167,63 @@ namespace SongBrowserPlugin
             _levelIdToCustomLevel = SongLoader.CustomLevels.ToDictionary(x => x.levelID, x => x);
 
             _log.Debug("Song Browser knows about {0} songs from SongLoader...", _sortedSongs.Count);
+        }
+
+        /// <summary>
+        /// Make the directory tree.
+        /// </summary>
+        /// <param name="customSongsPath"></param>
+        private void UpdateDirectoryTree(String customSongsPath)
+        {
+            // Determine folder mapping
+            Uri customSongDirUri = new Uri(customSongsPath);
+            _directoryTree = new Dictionary<string, DirectoryNode>();
+            _directoryTree[CUSTOM_SONGS_DIR] = new DirectoryNode(CUSTOM_SONGS_DIR);
+           
+            foreach (StandardLevelSO level in _originalSongs)
+            {
+                _log.Debug("Processing: {0}", level.levelID);
+                if (level.levelID.Length < 32) continue;
+                AddItemToDirectoryTree(customSongDirUri, level);
+            }
+        }
+
+        /// <summary>
+        /// Add a song to directory tree.  Determine its place in the tree by walking the split directory path.
+        /// </summary>
+        /// <param name="customSongDirUri"></param>
+        /// <param name="level"></param>
+        private void AddItemToDirectoryTree(Uri customSongDirUri, StandardLevelSO level)
+        {
+            DirectoryNode currentNode = _directoryTree[CUSTOM_SONGS_DIR];
+            CustomSongInfo songInfo = _levelIdToCustomLevel[level.levelID].customSongInfo;
+            Uri customSongUri = new Uri(songInfo.path);
+            Uri pathDiff = customSongDirUri.MakeRelativeUri(customSongUri);
+            string relPath = Uri.UnescapeDataString(pathDiff.OriginalString);
+            string[] paths = relPath.Split('/');
+
+            string curPath = paths[0];
+            for (int i = 1; i < paths.Length; i++)
+            {
+                string path = paths[i];
+
+                if (path == Path.GetFileName(songInfo.path))
+                {
+                    //_log.Debug("Done, adding {0} to {1}", level.levelID, currentNode.Key);
+                    currentNode.Levels.Add(level);
+                    break;
+                }
+                else if (currentNode.Nodes.ContainsKey(path))
+                {                    
+                    currentNode = currentNode.Nodes[path];
+                    //_log.Debug("Next node: {0}", currentNode.Key);
+                }
+                else
+                {
+                    //_log.Debug("Adding new node: {0}", path);
+                    currentNode.Nodes[path] = new DirectoryNode(path);
+                }
+            }
         }
         
         /// <summary>
@@ -160,32 +248,34 @@ namespace SongBrowserPlugin
             
             Stopwatch stopwatch = Stopwatch.StartNew();
 
+            List<StandardLevelSO> songList = _directoryTree[_currentDirectory].Levels;
+
             switch (_settings.sortMode)
             {
                 case SongSortMode.Favorites:
-                    SortFavorites();
+                    SortFavorites(songList);
                     break;
                 case SongSortMode.Original:
-                    SortOriginal();
+                    SortOriginal(songList);
                     break;
                 case SongSortMode.Newest:
-                    SortNewest();
+                    SortNewest(songList);
                     break;
                 case SongSortMode.Author:
-                    SortAuthor();
+                    SortAuthor(songList);
                     break;
                 case SongSortMode.PlayCount:
-                    SortPlayCount(gameplayMode);
+                    SortPlayCount(songList, gameplayMode);
                     break;
                 case SongSortMode.Random:
-                    SortRandom();
+                    SortRandom(songList);
                     break;
                 case SongSortMode.Search:
-                    SortSearch();
+                    SortSearch(songList);
                     break;
                 case SongSortMode.Default:
                 default:
-                    SortSongName();
+                    SortSongName(songList);
                     break;
             }
 
@@ -198,10 +288,10 @@ namespace SongBrowserPlugin
             _log.Info("Sorting songs took {0}ms", stopwatch.ElapsedMilliseconds);
         }    
         
-        private void SortFavorites()
+        private void SortFavorites(List<StandardLevelSO> levels)
         {
             _log.Info("Sorting song list as favorites");
-            _sortedSongs = _originalSongs
+            _sortedSongs = levels
                 .AsQueryable()
                 .OrderBy(x => _settings.favorites.Contains(x.levelID) == false)
                 .ThenBy(x => x.songName)
@@ -209,92 +299,92 @@ namespace SongBrowserPlugin
                 .ToList();
         }
 
-        private void SortOriginal()
+        private void SortOriginal(List<StandardLevelSO> levels)
         {
             _log.Info("Sorting song list as original");
-            _sortedSongs = _originalSongs
+            _sortedSongs = levels
                 .AsQueryable()
                 .OrderByDescending(x => _weights.ContainsKey(x.levelID) ? _weights[x.levelID] : 0)
                 .ThenBy(x => x.songName)
                 .ToList();
         }
 
-        private void SortNewest()
+        private void SortNewest(List<StandardLevelSO> levels)
         {
             _log.Info("Sorting song list as newest.");
-            _sortedSongs = _originalSongs
+            _sortedSongs = levels
                 .AsQueryable()
                 .OrderBy(x => _weights.ContainsKey(x.levelID) ? _weights[x.levelID] : 0)
                 .ThenByDescending(x => x.levelID.StartsWith("Level") ? _weights[x.levelID] : _cachedLastWriteTimes[_levelIdToCustomLevel[x.levelID].customSongInfo.path])
                 .ToList();
         }
 
-        private void SortAuthor()
+        private void SortAuthor(List<StandardLevelSO> levels)
         {
             _log.Info("Sorting song list by author");
-            _sortedSongs = _originalSongs
+            _sortedSongs = levels
                 .AsQueryable()
                 .OrderBy(x => x.songAuthorName)
                 .ThenBy(x => x.songName)
                 .ToList();
         }
 
-        private void SortPlayCount(GameplayMode gameplayMode)
+        private void SortPlayCount(List<StandardLevelSO> levels, GameplayMode gameplayMode)
         {
             _log.Info("Sorting song list by playcount");
             // Build a map of levelId to sum of all playcounts and sort.
             PlayerDynamicData playerData = GameDataModel.instance.gameDynamicData.GetCurrentPlayerDynamicData();
             IEnumerable<LevelDifficulty> difficultyIterator = Enum.GetValues(typeof(LevelDifficulty)).Cast<LevelDifficulty>();
-            Dictionary<string, int> _levelIdToPlayCount = _originalSongs.ToDictionary(x => x.levelID, x => difficultyIterator.Sum(difficulty => playerData.GetPlayerLevelStatsData(x.levelID, difficulty, gameplayMode).playCount));
-            _sortedSongs = _originalSongs
+            Dictionary<string, int> _levelIdToPlayCount = levels.ToDictionary(x => x.levelID, x => difficultyIterator.Sum(difficulty => playerData.GetPlayerLevelStatsData(x.levelID, difficulty, gameplayMode).playCount));
+            _sortedSongs = levels
                 .AsQueryable()
                 .OrderByDescending(x => _levelIdToPlayCount[x.levelID])
                 .ThenBy(x => x.songName)
                 .ToList();
         }
 
-        private void SortRandom()
+        private void SortRandom(List<StandardLevelSO> levels)
         {
             _log.Info("Sorting song list by random");
 
             System.Random rnd = new System.Random(Guid.NewGuid().GetHashCode());
 
-            _sortedSongs = _originalSongs
+            _sortedSongs = levels
                 .AsQueryable()
                 .OrderBy(x => rnd.Next())
                 .ToList();
         }
 
-        private void SortSearch()
+        private void SortSearch(List<StandardLevelSO> levels)
         {
             // Make sure we can actually search.
             if (this._settings.searchTerms.Count <= 0)
             {
                 _log.Error("Tried to search for a song with no valid search terms...");
-                SortSongName();
+                SortSongName(levels);
                 return;
             }
             string searchTerm = this._settings.searchTerms[0];
             if (String.IsNullOrEmpty(searchTerm))
             {
                 _log.Error("Empty search term entered.");
-                SortSongName();
+                SortSongName(levels);
                 return;
             }
 
             _log.Info("Sorting song list by search term: {0}", searchTerm);
             //_originalSongs.ForEach(x => _log.Debug($"{x.songName} {x.songSubName} {x.songAuthorName}".ToLower().Contains(searchTerm.ToLower()).ToString()));
 
-            _sortedSongs = _originalSongs
+            _sortedSongs = levels
                 .AsQueryable()
                 .Where(x => $"{x.songName} {x.songSubName} {x.songAuthorName}".ToLower().Contains(searchTerm.ToLower()))
                 .ToList();
         }
 
-        private void SortSongName()
+        private void SortSongName(List<StandardLevelSO> levels)
         {
             _log.Info("Sorting song list as default (songName)");
-            _sortedSongs = _originalSongs
+            _sortedSongs = levels
                 .AsQueryable()
                 .OrderBy(x => x.songName)
                 .ThenBy(x => x.songAuthorName)
