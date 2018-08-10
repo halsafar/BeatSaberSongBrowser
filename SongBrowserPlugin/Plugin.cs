31710eb3e0f78a4f38919a1cb59a5c8d83ba5c79
Stephen Damm <421822+halsafar@users.noreply.github.com>
2018-08-09 18:20:11 -0600

    Clean up tabs/spaces in this file.

--- SongBrowserPlugin/Plugin.cs
+++ SongBrowserPlugin/Plugin.cs
@@ -4,31 +4,31 @@ using IllusionPlugin;
 
 namespace SongBrowserPlugin
 {
-	public class Plugin : IPlugin
-	{
-		public string Name
-		{
-			get { return "Song Browser"; }
-		}
+    public class Plugin : IPlugin
+    {
+        public string Name
+        {
+            get { return "Song Browser"; }
+        }
 
-		public string Version
-		{
-			get { return "v2.2.3"; }
-		}
-		
-		public void OnApplicationStart()
-		{
+        public string Version
+        {
+            get { return "v2.2.3"; }
+        }
+
+        public void OnApplicationStart()
+        {
 
         }
 
-		public void OnApplicationQuit()
-		{      
+        public void OnApplicationQuit()
+        {
 
-		}
+        }
 
         private void SceneManagerOnActiveSceneChanged(Scene arg0, Scene scene)
         {
-            
+
         }
 
         private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
@@ -36,26 +36,26 @@ namespace SongBrowserPlugin
         }
 
         public void OnLevelWasLoaded(int level)
-		{          
+        {
             if (SceneManager.GetSceneByBuildIndex(level).name == "Menu")
             {
                 SongBrowserApplication.OnLoad();
             }
         }
 
-		public void OnLevelWasInitialized(int level)
-		{
+        public void OnLevelWasInitialized(int level)
+        {
 
         }
 
         public void OnUpdate()
-		{
-			
-		}
+        {
 
-		public void OnFixedUpdate()
-		{
-			
-		}
-	}
+        }
+
+        public void OnFixedUpdate()
+        {
+
+        }
+    }
 }
