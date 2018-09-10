c281e4bcd3ef8d50dc3cbc972c89c70c9156c3eb
Stephen Damm <421822+halsafar@users.noreply.github.com>
2018-09-09 21:31:44 -0600

    Version bump.

--- SongBrowserPlugin/Plugin.cs
+++ SongBrowserPlugin/Plugin.cs
@@ -13,7 +13,7 @@ namespace SongBrowserPlugin
 
         public string Version
         {
-            get { return "v2.2.7"; }
+            get { return "v2.2.8"; }
         }
 
         public void OnApplicationStart()
