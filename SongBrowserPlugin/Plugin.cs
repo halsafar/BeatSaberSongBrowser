70f017abef28a207000fd163d647163f28064c65
Stephen Damm <421822+halsafar@users.noreply.github.com>
2019-06-26 17:19:13 -0600

    Version bump.

--- SongBrowserPlugin/Plugin.cs
+++ SongBrowserPlugin/Plugin.cs
@@ -12,7 +12,7 @@ namespace SongBrowser
 {
     public class Plugin : IPlugin
     {
-        public const string VERSION_NUMBER = "5.2.0";
+        public const string VERSION_NUMBER = "5.2.1";
         public static Plugin Instance;
 
         public string Name
