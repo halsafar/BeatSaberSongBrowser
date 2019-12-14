bd5bfd20a20fe7a23bbbeccdb84934318dd80bfa
Stephen Damm <421822+halsafar@users.noreply.github.com>
2019-12-14 17:33:15 -0600

    Remove unused stuff, cleaned up code a bit.

--- SongBrowserPlugin/Internals/CustomHelpers.cs
+++ SongBrowserPlugin/Internals/CustomHelpers.cs
@@ -13,54 +13,6 @@ namespace SongBrowser.Internals
 {
     public static class CustomHelpers
     {
-
-        public static void RefreshTable(this TableView tableView, bool callbackTable = true)
-        {
-            HashSet<int> rows = new HashSet<int>(tableView.GetPrivateField<HashSet<int>>("_selectedCellIdxs"));
-            float scrollPosition = tableView.GetPrivateField<ScrollRect>("_scrollRect").verticalNormalizedPosition;
-
-            tableView.ReloadData();
-
-            tableView.GetPrivateField<ScrollRect>("_scrollRect").verticalNormalizedPosition = scrollPosition;
-            tableView.SetPrivateField("_targetPosition", scrollPosition);
-            if (rows.Count > 0)
-                tableView.SelectCellWithIdx(rows.First(), callbackTable);
-        }
-        //bananbread levelpacks
-
-        public static SongCoreCustomBeatmapLevelPack GetLevelPackWithLevels(CustomPreviewBeatmapLevel[] levels, string packName = null, Sprite packCover = null, string packID = null)
-        {
-
-            SongCoreCustomLevelCollection levelCollection = new SongCoreCustomLevelCollection(levels.ToArray());
-
-
-            SongCoreCustomBeatmapLevelPack pack = new SongCoreCustomBeatmapLevelPack(string.IsNullOrEmpty(packID) ? "" : packID,
-                string.IsNullOrEmpty(packName) ? "Custom Songs" : packName, packCover ?? Sprites.BeastSaberLogo, levelCollection);
-            //       pack.SetPrivateField("_packName", string.IsNullOrEmpty(packName) ? "Custom Songs" : packName);
-            //       pack.SetPrivateField("_coverImage", packCover ?? Sprites.BeastSaberLogo);
-            //       pack.SetPrivateField("_packID", string.IsNullOrEmpty(packID) ? "" : packID);
-            //       pack.SetPrivateField("_isPackAlwaysOwned", true);
-
-            return pack;
-
-        }
-
-
-        static char[] hexChars = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-
-        public static string CheckHex(string input)
-        {
-            input = input.ToUpper();
-            if (input.All(x => hexChars.Contains(x)))
-            {
-                return input;
-            }
-            else
-            {
-                return "";
-            }
-        }
-
         public static bool IsModInstalled(string ModName)
         {
             foreach (var mod in IPA.Loader.PluginManager.Plugins)
@@ -82,11 +34,10 @@ namespace SongBrowser.Internals
             return split.Length > 2 ? split[2] : levelId;
         }
 
-        private const BindingFlags _allBindingFlags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
         public static object GetField(this object obj, string fieldName)
         {
             return (obj is Type ? (Type)obj : obj.GetType())
-                .GetField(fieldName, _allBindingFlags)
+                .GetField(fieldName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
                 .GetValue(obj);
         }   
     }
