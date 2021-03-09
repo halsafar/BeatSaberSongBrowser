8d30faf60c3645ca076e8196fd8294e4d15875ad
Halsafar <421822+halsafar@users.noreply.github.com>
2021-03-09 15:57:09 -0600

    Adapt fixes from SongCore ProgressBar.

--- SongBrowserPlugin/UI/ProgressBar.cs
+++ SongBrowserPlugin/UI/ProgressBar.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
+﻿using SongCore.Utilities;
+using System.Collections;
 using System.Collections.Concurrent;
-using HMUI;
-using SongBrowser.Internals;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using SongCore;
 
 namespace SongBrowser.UI
 {
@@ -19,8 +19,9 @@ namespace SongBrowser.UI
         private TMP_Text _pluginNameText;
         private TMP_Text _headerText;
         internal Image _loadingBackg;
+        internal Image _loadingBar;
 
-        private static readonly Vector3 Position = new Vector3(0, 0.0f, 2.6f);
+        private static readonly Vector3 Position = new Vector3(0, 0, 2.5f);
         private static readonly Vector3 Rotation = Vector3.zero;
         private static readonly Vector3 Scale = new Vector3(0.01f, 0.01f, 0.01f);
 
@@ -30,7 +31,7 @@ namespace SongBrowser.UI
         private const float AuthorNameFontSize = 7f;
         private static readonly Vector2 AuthorNamePosition = new Vector2(10, 31);
 
-        private string PluginNameText => $"Song Browser - v<size=100%>{Plugin.VersionNumber}</size>";
+        private string PluginNameText = $"Song Browser - v<size=100%>{Plugin.VersionNumber}</size>";
         private const float PluginNameFontSize = 9f;
         private static readonly Vector2 PluginNamePosition = new Vector2(10, 23);
 
@@ -54,8 +55,9 @@ namespace SongBrowser.UI
             StopAllCoroutines();
             _showingMessage = true;
             _headerText.text = message;
+            _loadingBar.enabled = false;
             _loadingBackg.enabled = false;
-            gameObject.SetActive(true);
+            _canvas.enabled = true;
             StartCoroutine(DisableCanvasRoutine(time));
         }
 
@@ -64,8 +66,9 @@ namespace SongBrowser.UI
             StopAllCoroutines();
             _showingMessage = true;
             _headerText.text = message;
+            _loadingBar.enabled = false;
             _loadingBackg.enabled = false;
-            gameObject.SetActive(true);
+            _canvas.enabled = true;
         }
 
         private void OnEnable()
@@ -77,7 +80,7 @@ namespace SongBrowser.UI
         private void OnDisable()
         {
             SceneManager.activeSceneChanged -= SceneManagerOnActiveSceneChanged;
-            SongBrowserModel.didFinishProcessingSongs -= SongBrowserFinishedProcessingSongs;
+            SongBrowserModel.didFinishProcessingSongs -= SongBrowserFinishedProcessingSongs;               
         }
 
         private void SceneManagerOnActiveSceneChanged(Scene oldScene, Scene newScene)
@@ -86,68 +89,104 @@ namespace SongBrowser.UI
             {
                 if (_showingMessage)
                 {
-                    gameObject.SetActive(true);
+                    _canvas.enabled = true;
                 }
             }
             else
             {
-                gameObject.SetActive(false);
+                _canvas.enabled = false;
             }
         }
 
-        private void SongBrowserFinishedProcessingSongs(ConcurrentDictionary<string, CustomPreviewBeatmapLevel> arg2)
+        private void SongLoaderOnLoadingStartedEvent(Loader obj)
         {
             StopAllCoroutines();
             _showingMessage = false;
-            _headerText.text = arg2.Count + " songs processed";
+            _headerText.text = HeaderText;
+            _loadingBar.enabled = true;
+            _loadingBackg.enabled = true;
+            _canvas.enabled = true;
+        }
+
+        private void SongBrowserFinishedProcessingSongs(ConcurrentDictionary<string, CustomPreviewBeatmapLevel> customLevels)
+        {
+            _showingMessage = false;
+            _headerText.text = customLevels.Count + " songs processed.";
+            _loadingBar.enabled = false;
             _loadingBackg.enabled = false;
-            StartCoroutine(DisableCanvasRoutine(20f));
+            StartCoroutine(DisableCanvasRoutine(7f));
         }
 
         private IEnumerator DisableCanvasRoutine(float time)
         {
             yield return new WaitForSecondsRealtime(time);
+            _canvas.enabled = false;
             _showingMessage = false;
-            gameObject.SetActive(false);
         }
 
         private void Awake()
         {
+            gameObject.transform.position = Position;
+            gameObject.transform.eulerAngles = Rotation;
+            gameObject.transform.localScale = Scale;
+
             _canvas = gameObject.AddComponent<Canvas>();
             _canvas.renderMode = RenderMode.WorldSpace;
-            
-            gameObject.AddComponent<CurvedCanvasSettings>().SetRadius(0f);
+            _canvas.enabled = false;
+            var rectTransform = _canvas.transform as RectTransform;
+            rectTransform.sizeDelta = CanvasSize;
 
-            var ct = _canvas.transform;
-            ct.position = Position;
-            ct.localScale = Scale;
+            _authorNameText = BeatSaberMarkupLanguage.BeatSaberUI.CreateText(_canvas.transform as RectTransform, AuthorNameText, AuthorNamePosition);
+            rectTransform = _authorNameText.transform as RectTransform;
+            rectTransform.SetParent(_canvas.transform, false);
+            rectTransform.anchoredPosition = AuthorNamePosition;
+            rectTransform.sizeDelta = HeaderSize;
+            _authorNameText.text = AuthorNameText;
+            _authorNameText.fontSize = AuthorNameFontSize;
 
-            if (ct is RectTransform crt)
-            {
-                crt.sizeDelta = CanvasSize;
+            _pluginNameText = BeatSaberMarkupLanguage.BeatSaberUI.CreateText(_canvas.transform as RectTransform, PluginNameText, PluginNamePosition);
+            rectTransform = _pluginNameText.transform as RectTransform;
+            rectTransform.SetParent(_canvas.transform, false);
+            rectTransform.sizeDelta = HeaderSize;
+            rectTransform.anchoredPosition = PluginNamePosition;
+            _pluginNameText.text = PluginNameText;
+            _pluginNameText.fontSize = PluginNameFontSize;
 
-                _authorNameText = BeatSaberUI.CreateText(crt, AuthorNameText, AuthorNameFontSize, AuthorNamePosition, HeaderSize);
+            _headerText = BeatSaberMarkupLanguage.BeatSaberUI.CreateText(_canvas.transform as RectTransform, HeaderText, HeaderPosition);
+            rectTransform = _headerText.transform as RectTransform;
+            rectTransform.SetParent(_canvas.transform, false);
+            rectTransform.anchoredPosition = HeaderPosition;
+            rectTransform.sizeDelta = HeaderSize;
+            _headerText.text = HeaderText;
+            _headerText.fontSize = HeaderFontSize;
 
-                _pluginNameText = BeatSaberUI.CreateText(crt, PluginNameText, PluginNameFontSize, PluginNamePosition, HeaderSize);
-
-                _headerText = BeatSaberUI.CreateText(crt, HeaderText, HeaderFontSize, HeaderPosition, HeaderSize);
-            }
-
-            _loadingBackg = new GameObject("Background").AddComponent<ImageView>();
-            if (_loadingBackg.transform is RectTransform lbrt)
-            {
-                lbrt.SetParent(_canvas.transform, false);
-                lbrt.sizeDelta = LoadingBarSize;
-            }
-
+            _loadingBackg = new GameObject("Background").AddComponent<Image>();
+            rectTransform = _loadingBackg.transform as RectTransform;
+            rectTransform.SetParent(_canvas.transform, false);
+            rectTransform.sizeDelta = LoadingBarSize;
             _loadingBackg.color = BackgroundColor;
 
-            gameObject.SetActive(false);
+            _loadingBar = new GameObject("Loading Bar").AddComponent<Image>();
+            rectTransform = _loadingBar.transform as RectTransform;
+            rectTransform.SetParent(_canvas.transform, false);
+            rectTransform.sizeDelta = LoadingBarSize;
+            var tex = Texture2D.whiteTexture;
+            var sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.one * 0.5f, 100, 1);
+            _loadingBar.sprite = sprite;
+            _loadingBar.type = Image.Type.Filled;
+            _loadingBar.fillMethod = Image.FillMethod.Horizontal;
+            _loadingBar.color = new Color(1, 1, 1, 0.5f);
+
+            DontDestroyOnLoad(gameObject);
         }
 
         private void Update()
         {
             if (!_canvas.enabled) return;
+            _loadingBar.fillAmount = Loader.LoadingProgress;
+
+            _loadingBar.color = HSBColor.ToColor(new HSBColor(Mathf.PingPong(Time.time * 0.35f, 1), 1, 1));
+            _headerText.color = HSBColor.ToColor(new HSBColor(Mathf.PingPong(Time.time * 0.35f, 1), 1, 1));
         }
     }
 }
