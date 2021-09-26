d75e5ae99448f67287a37e2aabe9ffafc08b0d9d
PixelBoom <rithikbhatia12@gmail.com>
2021-09-26 04:30:06 -0400

    Removed playlist path code
    This is now a part of BeatsaberPlaylistsLib, no longer needed here

--- SongBrowserPlugin/DataAccess/Playlist.cs
+++ SongBrowserPlugin/DataAccess/Playlist.cs
@@ -10,29 +10,12 @@ namespace SongBrowser.DataAccess
 
         public static BeatSaberPlaylistsLib.Types.IPlaylist CreateNew(string playlistName, IPreviewBeatmapLevel[] beatmapLevels)
         {
-            string playlistFolderPath = defaultManager.PlaylistPath;
-            string playlistFileName = string.Join("_", playlistName.Replace("/", "").Replace("\\", "").Replace(".", "").Split(' '));
-            if (string.IsNullOrEmpty(playlistFileName))
-            {
-                playlistFileName = "playlist";
-            }
-            string playlistPath = Path.Combine(playlistFolderPath, playlistFileName + ".blist");
-            string originalPlaylistPath = Path.Combine(playlistFolderPath, playlistFileName);
-            int dupNum = 0;
-            while (File.Exists(playlistPath))
-            {
-                dupNum++;
-                playlistPath = originalPlaylistPath + string.Format("({0}).blist", dupNum);
-                playlistFileName += string.Format("({0})", dupNum);
-            }
-
-            BeatSaberPlaylistsLib.Types.IPlaylist playlist = defaultManager.CreatePlaylist(playlistFileName, playlistName, "SongBrowser", "");
+            BeatSaberPlaylistsLib.Types.IPlaylist playlist = defaultManager.CreatePlaylist("", playlistName, "SongBrowser", "");
             foreach (var beatmapLevel in beatmapLevels)
             {
                 playlist.Add(beatmapLevel);
             }
             defaultManager.StorePlaylist(playlist);
-            playlist.SuggestedExtension = defaultManager.DefaultHandler?.DefaultExtension;
             return playlist;
         }
     }
